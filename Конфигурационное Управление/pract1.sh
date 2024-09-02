510be073b593214269d1d3f9b42cacac1ab3973a
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2024-09-02 20:30:51 +0300

    Update pract1.sh

--- Конфигурационное Управление/pract1.sh
+++ Конфигурационное Управление/pract1.sh
@@ -5,4 +5,16 @@ grep '^[^:]' /etc/passwd | cut -d: -f1 | sort
 cat /etc/protocols | awk '{print $2, $1}' | sort -nr | head -n5
 
 Задача 3:
+#!/bin/bash
 
+text=$*
+length=${#text}
+
+# Формирование символов рамки от 1 до длины слова + 2 пробела
+for _ in $(seq 1 $((length + 2))); do
+    line+="-"
+done
+
+echo "+${line}+"
+echo "| ${text} |"
+echo "+${line}+"
