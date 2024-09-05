c9aa82ce5d54591555f666990620ab6472db2c2d
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2024-09-05 10:18:30 +0300

    Update 5.1.cpp

--- СИАОД/5.1.cpp
+++ СИАОД/5.1.cpp
@@ -45,3 +45,25 @@ int main()
 
 
 Задание 2б:
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {1, 0, 5, 7, 2, 4};
+
+    unsigned char maska = 1;
+    unsigned char ArrayBit = 0;
+
+    for (int i: arr) {
+        ArrayBit = ArrayBit | (maska << i);
+    }
+    
+    for (int i = 0; i < 8; i++) {
+        if (ArrayBit & (1 << i)) {
+            cout << i << " ";
+        }
+    }
+    
+    return 0;
+}
