c985f1cacc847129d8d3a56cfb428c83dbd6538a
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2024-09-10 22:33:18 +0300

    Update 5.1.cpp

--- СИАОД/5.1.cpp
+++ СИАОД/5.1.cpp
@@ -1,19 +1,23 @@
 Задание 1а:
+#include <iostream>
+using namespace std;
 int main() {
-  unsigned char x = 255;
-  unsigned char maska = 1;
-  x = x & (~(maska << 4));
-  cout << (int)x;
-  return 0;
+	unsigned char x = 255;//8-разрядное двоичное число 11111111
+	unsigned char maska = 1;//1=00000001 – 8-разрядная маска
+	x = x & (~(maska << 4));//результат x = 239
+	cout << (int)x;
+	return 0;
 }
 
 Задание 1б:
+#include <iostream>
+using namespace std;
 int main() {
-  unsigned char x = 255;
-  unsigned char maska = 1;
-  x = x & (maska << 6);
-  cout << (int)x;
-  return 0;
+	unsigned char x = 255;//8-разрядное двоичное число 11111111
+	unsigned char maska = 1;//1=00000001 – 8-разрядная маска
+	x = x & (maska << 6);//результат x = 64
+	cout << (int)x;
+	return 0;
 }
 
 Задание 1в:
