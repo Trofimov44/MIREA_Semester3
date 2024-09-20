ce821b5523495f8c395c8327ee4760b6a3b26842
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2024-09-20 15:16:08 +0300

    Update main.java

--- Программирование на Джава/Практика1/main.java
+++ Программирование на Джава/Практика1/main.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class test1 {
+    public static void main(String[] args){
+        int yuan;
+        double roubles;
+        final float ROUBLES_PER_YUAN = 11.91f;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите Кол-во Юаней: ");
+        yuan = scanner.nextInt();
+
+        roubles = ROUBLES_PER_YUAN * yuan;
+        roubles = Math.ceil(roubles);
+
+        double digit = yuan % 10;
+
+        if (yuan == 1){
+            System.out.println("У вас 1 китайский Юань, " + "это " + roubles + " руб");
+        }else if ( digit > 0 && digit < 5 && (yuan > 19 || yuan < 5) && yuan != 0) {
+             System.out.println("У вас "+ yuan + " китайских Юаня - " + "это " + roubles + " руб");
+        }else {
+            System.out.println("У вас "+ yuan + " китайских Юаней - " + "это " + roubles + " руб");
+        }
+    }
+}
