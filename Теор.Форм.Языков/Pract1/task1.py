654fc147b67c90d102aee8ecdf10cc740dab0c19
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2024-09-21 14:35:53 +0300

    Update task1.py

--- Теор.Форм.Языков/Pract1/task1.py
+++ Теор.Форм.Языков/Pract1/task1.py
@@ -1,1 +1,34 @@
+def OPZ(expression):
+    output = []  # Список для хранения выходного выражения
+    stack = []   # Стек для хранения операторов и скобок
+    # Обрабатываем каждый токен в выражении
+    for token in expression.split():
+        if token.isalnum():  # Если токен - число или переменная
+            output.append(token)  
+        elif token == '(': # Если токен - открывающая скобка
+            stack.append(token)  
+        elif token == ')':  # Если токен - закрывающая скобка
+            while stack and stack[-1] != '(':
+                output.append(stack.pop())
+            stack.pop() 
+        else:  # Если токен - оператор
+            while (stack and priority(stack[-1]) >= priority(token)):
+                output.append(stack.pop())
+            stack.append(token)  # Добавляем текущий оператор в стек
 
+    # Выталкиваем все оставшиеся операторы из стека в выходной список
+    while stack:
+        output.append(stack.pop())
+    return ' '.join(output)  # Возвращаем результат в виде строки
+
+def priority(op):
+    # Определяет приоритет операторов
+    if op == '+' or op == '-':
+        return 1
+    if op == '*' or op == '/':
+        return 2
+    return 0
+
+expression = input("Введите алгебраическое выражение, разделяя пробелами: ")
+result = OPZ(expression)
+print(result)  # Вывод
