9d7cf59315c1407a8e47abccb879426971114632
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2024-09-21 14:36:15 +0300

    Update task2.py

--- Теор.Форм.Языков/Pract1/task2.py
+++ Теор.Форм.Языков/Pract1/task2.py
@@ -1,1 +1,28 @@
+def evaluate_rpn(expression):
+    stack = []
 
+    for token in expression.split():
+        if token.isdigit() or (token[0] == '-' and token[1:].isdigit()):
+            # Если токен - число, добавляем его в стек
+            stack.append(int(token))
+        else:
+            # Если токен - оператор, извлекаем два последних числа из стека
+            b = stack.pop()
+            a = stack.pop()
+
+            if token == '+':
+                stack.append(a + b)
+            elif token == '-':
+                stack.append(a - b)
+            elif token == '*':
+                stack.append(a * b)
+            elif token == '/':
+                stack.append(a // b)  # Целочисленное деление
+
+    # В конце в стеке должно остаться одно число - результат
+    return stack[0]
+
+# Пример использования
+expression = "3 4 + 2 * 7 /"
+result = evaluate_rpn(expression)
+print(f"Результат выражения '{expression}' = {result}")
