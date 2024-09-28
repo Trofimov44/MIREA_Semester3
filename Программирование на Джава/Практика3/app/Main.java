ac3f8e3004447bdb417cc36be92c20112a63c42f
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2024-09-28 09:45:35 +0300

    Update Main.java

--- Программирование на Джава/Практика3/app/Main.java
+++ Программирование на Джава/Практика3/app/Main.java
@@ -6,18 +6,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        System.out.println();
         Car car2 = new Car("ownerNamecar2", 2, "engineType");
-        ElectricCar car3 = new ElectricCar("ownerName", 3, 2000);
+        ElectricCar car3 = new ElectricCar("ownerName3", 3, 2000);
 
         System.out.println(car2.getOwnerName());
         System.out.println(car2.getInsuranceNumber());
-
-        car3.setOwnerName("ownerNamecar3");
+        System.out.println(car2.getEngineType());
+        System.out.println(" ");
         System.out.println(car3.getOwnerName());
         System.out.println(car3.getInsuranceNumber());
-        
         System.out.println(car3.getEngineType());
-
+        System.out.println(car3.getBatteryCapacity());
     }
 }
