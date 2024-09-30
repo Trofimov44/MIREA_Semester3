d6e232ad7e032fac95f4f743682448d09dbe5ff4
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2024-09-30 16:55:30 +0300

    Create ElectricCar.java

---
+++ Программирование на Джава/Практика5/vehicles/ElectricCar.java
@@ -0,0 +1,29 @@
+package vehicles;
+
+public class ElectricCar extends Car implements ElectricVehicle {
+    private int batteryCapacity;
+
+    public ElectricCar(String model,int license,String color,int year,String ownerName, int insuranceNumber,int batteryCapacity) {
+        super(model,license,color,year,ownerName,insuranceNumber,null);
+        this.engineType = "electric";
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    public String vehicleType(){
+        return "Electric Car";
+    }
+
+    public int getBatteryCapacity() {
+        return batteryCapacity;
+    }
+
+    public void setBatteryCapacity(int batteryCapacity){
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    public String toString() {
+        return "{" + getModel() + " " + getLicense() + " " + getColor() +
+                " " + getYear() + " " + getOwnerName() + " " + getInsuranceNumber() +
+                " " + engineType + " " + batteryCapacity + "}";
+    }
+}
