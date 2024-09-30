5f452836c61b0bca80f5186f64c953c02948c5c9
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2024-09-30 16:53:42 +0300

    Create Main.java

---
+++ Программирование на Джава/Практика5/app/Main.java
@@ -0,0 +1,25 @@
+package app;
+import vehicles.Car;
+import vehicles.ElectricCar;
+import vehicles.Vehicle;
+import java.util.Scanner;
+
+public class Main { 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        Car car1 = new Car("Lada", 22, "red", 2, "Tim", 32, "eT");
+        ElectricCar car2 = new ElectricCar("Mockvich", 44, "blue", 1, "B", 122,2000);
+
+        car1.setYear(4);
+        car2.setOwnerName("Vlad");
+
+        car2.setInsuranceNumber(2233);
+
+        System.out.println(car2.getBatteryCapacity());
+        System.out.println(car1.toString());
+        System.out.println(car2.toString());
+
+    }
+}
+
