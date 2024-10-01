cf9a7591ed469df0ebdf3699d467b88f84b76c42
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2024-10-01 12:54:54 +0300

    Update Car.java

--- Программирование на Джава/Практика5/vehicles/Car.java
+++ Программирование на Джава/Практика5/vehicles/Car.java
@@ -2,8 +2,9 @@ package vehicles;
 
 public class Car extends Vehicle{
 
-    public Car(String model,int license,String color,int year,String ownerName, int insuranceNumber, String engineType){
-        super(model,license,color,year,ownerName,insuranceNumber,engineType);
+    public Car(String model,int license,String color,int year,String ownerName, int insuranceNumber){
+        super(model,license,color,year,ownerName,insuranceNumber,null);
+        this.engineType = "Combustion";
     }
 
     public String vehicleType(){
