1e04446395e66c73ead64402a1f56f08a9aa2fec
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2024-10-11 15:17:00 +0300

    Update TestCar.java

--- Программирование на Джава/Практика5/app/TestCar.java
+++ Программирование на Джава/Практика5/app/TestCar.java
@@ -6,9 +6,8 @@ import java.util.Scanner;
 
 public class TestCar {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-        Car car1 = new Car("Lada", 22, "red", 2, "Tim", 32, "eT");
+        Car car1 = new Car("Lada", 22, "red", 2, "Tim", 32);
         ElectricCar car2 = new ElectricCar("Mockvich", 44, "blue", 1, "B", 122,2000);
 
         car1.setYear(4);
@@ -16,7 +15,7 @@ public class TestCar {
 
         car2.setInsuranceNumber(2233);
 
-        System.out.println(car2.getBatteryCapacity());
+        System.out.println("BatteryCapacity = " + car2.getBatteryCapacity());
 
         System.out.println(car1.toString());
         System.out.println(car2.toString());
