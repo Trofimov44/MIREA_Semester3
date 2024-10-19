2e74db35a7a9877beab8647eb3339ee7642129c9
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2024-10-19 15:18:47 +0300

    Update 1б.cpp

--- СИАОД/5.1/1б.cpp
+++ СИАОД/5.1/1б.cpp
@@ -1,1 +1,9 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+	unsigned char x = 255;//8-разрядное двоичное число 11111111
+	unsigned char maska = 1;//1=00000001 – 8-разрядная маска
+	x = x & (maska << 6);//результат x = 64
+	cout << (int)x;
+	return 0;
+}
