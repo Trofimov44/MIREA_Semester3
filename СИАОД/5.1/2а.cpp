2bcc748cbbbe8e6f41f1d6aeeb1275d0cb716a41
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2024-10-19 15:19:14 +0300

    Update 2а.cpp

--- СИАОД/5.1/2а.cpp
+++ СИАОД/5.1/2а.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int arr[] = {1, 0, 5, 7, 2, 4};
+
+    unsigned char maska = 1;
+    unsigned char ArrayBit = 0;
+
+    for (int i: arr) {
+        ArrayBit = ArrayBit | (maska << i);
+    }
+    
+    for (int i = 0; i < 8; i++) {
+        if (ArrayBit & (maska << i)) {
+            cout << i << " ";
+        }
+    }
+    
+    return 0;
+}
