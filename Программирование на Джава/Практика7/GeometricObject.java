cdce3566ad3c1a1aac9bbd5596cdcaac0ccab675
Trofimov44 <73187280+Trofimov44@users.noreply.github.com>
2024-10-19 15:00:50 +0300

    Create GeometricObject.java

---
+++ Программирование на Джава/Практика7/GeometricObject.java
@@ -0,0 +1,73 @@
+public class GeometricObject implements Comparable, Colorable{
+    private String color = "белый";
+    private boolean filled;
+    private java.util.Date dateCreated;
+
+    /** Создает по умолчанию заданный геометрический объект */
+    public GeometricObject() {
+        dateCreated = new java.util.Date();
+    }
+
+    /** Создает геометрический объект с указанным цветом и заливкой */
+    public GeometricObject(String color, boolean filled) {
+        dateCreated = new java.util.Date();
+        this.color = color;
+        this.filled = filled;
+    }
+
+    /** Возвращает цвет */
+    public String getColor() {
+        return color;
+    }
+
+    /** Присваивает новый цвет */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /** Возвращает заливку. Поскольку filled типа boolean,
+     *  getter-метод называется isFilled */
+    public boolean isFilled() {
+        return filled;
+    }
+
+    /** Присваивает новую заливку */
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    /** Получает dateCreated */
+    public java.util.Date getDateCreated() {
+        return dateCreated;
+    }
+
+    /** Возвращает строковое представление этого объекта */
+    public String toString() {
+        return "создан " + dateCreated + ",\nцвет: " + color +
+                ", заливка: " + filled;
+    }
+
+    public static String max(GeometricObject o1, GeometricObject o2){
+        return "Что вы хотите чтобы тут возвращалось?У этого класса даже размера для объектов нет";
+    }
+
+    public static String compareTo(Circle o1, Rectangle o2){
+        if (o1.getArea() > o2.getArea()){
+            return "Круг больше квадрата ";
+        } else if (o1.getArea() < o2.getArea()) {
+            return "Квадрат больше круга ";
+        }
+        else{
+            return "Квадрат и круг равны ";
+        }
+    }
+
+    public double getArea() {
+        return 0;
+    }
+
+
+    public void howToColor() {
+        System.out.println("Его не раскрасить.");
+    }
+}
